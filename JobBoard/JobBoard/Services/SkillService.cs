a4d730c4e2ac8b21f8aa4b8861461bf20acb9726
Kevin <kevin.michael.horan@gmail.com>
2017-05-06 22:55:13 -0700

    re-added dropped files

---
+++ JobBoard/JobBoard/Services/SkillService.cs
@@ -0,0 +1,54 @@
+﻿using JobBoard.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobBoard.Services
+{
+    public class SkillService
+    {
+        public static SkillDomain GetCs(int level)
+        {
+            SkillDomain skill = new SkillDomain { Skill = "C#", Level = level };
+
+            return skill;
+        }
+
+        public static SkillDomain GetHtml(int level)
+        {
+            SkillDomain skill = new SkillDomain { Skill = "HTML", Level = level };
+
+            return skill;
+        }
+
+        public static SkillDomain GetJS(int level)
+        {
+            SkillDomain skill = new SkillDomain { Skill = "JavaScript", Level = level };
+
+            return skill;
+        }
+
+        public static SkillDomain GetCSS(int level)
+        {
+            SkillDomain skill = new SkillDomain { Skill = "CSS", Level = level };
+
+            return skill;
+        }
+
+        public static SkillDomain GetAngular(int level)
+        {
+            SkillDomain skill = new SkillDomain { Skill = "AngularJS", Level = level };
+
+            return skill;
+        }
+
+
+        public static SkillDomain GetSQL(int level)
+        {
+            SkillDomain skill = new SkillDomain { Skill = "SQL", Level = level };
+
+            return skill;
+        }
+    }
+}
